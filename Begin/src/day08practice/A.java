af04dfda5f6cd9273ba9a6411ff5e2217ff2de8e
Bomi <aloe06@gmail.com>
2022-07-12 18:10:42 +0900

    test

---
+++ Begin/src/day08practice/A.java
@@ -0,0 +1,21 @@
+package day08practice;
+//[연습문제8.문제3]
+class A {
+	public int getNumber(int a) {
+		return a+1;
+	}
+}
+
+class B extends A{
+	public int getNumber(int a) {
+		return a+2;
+	}
+	
+	public static void main(String[] args) {
+		A a=new B();
+		System.out.println(a.getNumber(0));
+
+	}
+
+}
+//오버로딩되어 B의 getNuber로 작동해 답2
