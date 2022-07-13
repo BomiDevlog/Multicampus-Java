e617842e5b261f06763e4296e3e24bdda83392c8
Bomi <aloe06@gmail.com>
2022-07-13 17:11:30 +0900

    multiple test

---
+++ Begin/src/practice/MultipleTest.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class MultipleTest {
+
+	public static void main(String[] args) {
+		/*1~100까지의 정수 중에서
+		 * 3 또는 4의 배수의 합을 계산
+		 */
+		int sum=0;
+		for(int i=1;i<101;i++) {
+			if(i%3==0 || i%4==0) {
+				sum+=i;
+				System.out.println("3 또는 4의 배수의 합"+sum);
+			}
+		}
+
+	}
+
+}
